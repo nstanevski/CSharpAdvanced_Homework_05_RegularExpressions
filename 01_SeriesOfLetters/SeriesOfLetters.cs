771b7001c8c949e7e82440168511a61d0941879b
nstanevski <nikolay.stanevski@gmail.com>
2015-05-13 23:31:43 +0300

    tasks ## 1,2 - finished

--- 01_SeriesOfLetters/SeriesOfLetters.cs
+++ 01_SeriesOfLetters/SeriesOfLetters.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
-
 class SeriesOfLetters
 {
     static void Main()
     {
         string input = Console.ReadLine();
         string pattern = @"(\w)\1+";
-        string replacement = "$1";  
+        string replacement = "$1";
         string result = Regex.Replace(input, pattern, replacement);
         Console.WriteLine(result);
     }
