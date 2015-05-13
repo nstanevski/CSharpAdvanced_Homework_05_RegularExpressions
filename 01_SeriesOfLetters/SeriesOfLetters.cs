3bed97bf7338d025d096762543ba7970df498a17
nstanevski <nikolay.stanevski@gmail.com>
2015-05-13 21:42:30 +0300

    task#1

---
+++ 01_SeriesOfLetters/SeriesOfLetters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+class SeriesOfLetters
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string pattern = @"(\w)\1+";
+        string replacement = "$1";  
+        string result = Regex.Replace(input, pattern, replacement);
+        Console.WriteLine(result);
+    }
+}
