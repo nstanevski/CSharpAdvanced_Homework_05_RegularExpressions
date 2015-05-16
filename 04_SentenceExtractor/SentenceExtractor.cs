e778d0c0516ba0bf4856fb543d6177fbf5a0c152
nstanevski <nikolay.stanevski@gmail.com>
2015-05-16 08:44:25 +0300

    tasks #7

---
+++ 04_SentenceExtractor/SentenceExtractor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/*
+ * Write a program that reads a keyword and some text from the console and prints
+ * all sentences from the text, containing that word. A sentence is any sequence
+ * of words ending with '.', '!' or '?'. 
+ */
+
+class SentenceExtractor
+{
+    static void Main()
+    {
+        string keyword = Console.ReadLine().Trim();
+        keyword = @"\b"+keyword+@"\b";
+        string text = Console.ReadLine();
+
+        // \u2019 and \u2013 are en-dash and apostrophy
+        string sentencePattern = @"[A-Z][A-Za-z\s\-\,\:\'\u2019\u2013]*[\.\?\!]";
+        Regex sentenceRegex = new Regex(sentencePattern);
+        MatchCollection matches = sentenceRegex.Matches(text);
+        foreach (Match match in matches)
+        {
+            string sentence = match.Value;
+            if (Regex.IsMatch(sentence, keyword))
+                Console.WriteLine(sentence);
+        }
+    }
+}
---
+++ 07_QuerryMess/QuerryMess.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+class QuerryMess
+{
+    private static Dictionary<string, List<string>> dict =
+        new Dictionary<string, List<string>>();
+
+    private static void ProcessQuery(string line)
+    {
+        string url = @"^[^>?]+\?"; //catch and remove leading part, up to '?'
+        Regex urlRegex = new Regex(url);
+        string res = urlRegex.Replace(line, "");
+
+        string[] pairsStr = res.Split('&');
+        foreach (string pairStr in pairsStr)
+        {
+            //replace encoded spaces with literal ones:
+            string spaceEnc = @"(\+|%20)";
+            res = Regex.Replace(pairStr, spaceEnc, " ");
+            string[] keyValueAttr = res.Split('=');
+            string key = keyValueAttr[0].Trim();
+            string value = keyValueAttr[1].Trim();
+            if (dict.ContainsKey(key))
+                dict[key].Add(value);
+            else
+            {
+                List<string> values = new List<string>();
+                values.Add(value);
+                dict[key] = values;
+            }
+        }
+
+    }
+
+    static void Main()
+    {
+        string line;
+        do{
+            line = Console.ReadLine();
+            if (line == "END")
+                break;
+            ProcessQuery(line);
+            foreach (KeyValuePair<string, List<string>> pair in dict)
+            {
+                string valueStr = string.Join(", ", pair.Value.ToArray());
+                Console.Write("{0}=[{1}]", pair.Key, valueStr);
+            }
+            dict.Clear();
+            Console.WriteLine();
+
+        }while(true);
+    } 
+}
